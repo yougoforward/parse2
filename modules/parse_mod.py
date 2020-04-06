dd4dbdd87dbfb0b5979bf9816f4ebfbd2bc0d4dd
yougoforward <908865817@qq.com>
2020-04-07 07:33:40 +0800

    aspp

--- modules/parse_mod.py
+++ modules/parse_mod.py
@@ -102,25 +102,25 @@ class ASPPModule(nn.Module):
         n, c, h, w = feat0.size()
 
         # psaa
-        y1 = torch.cat((feat0, feat1, feat2, feat3), 1)
+        # y1 = torch.cat((feat0, feat1, feat2, feat3), 1)
         # psaa_feat = self.psaa_conv(torch.cat([x, y1], dim=1))
         # psaa_att = torch.sigmoid(psaa_feat)
         # psaa_att_list = torch.split(psaa_att, 1, dim=1)
 
         # y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2, psaa_att_list[3] * feat3), 1)
-        out = self.project(y1)
+        # out = self.project(y1)
 
         #gp
-        gp = self.gap(x)
-        se = self.se(gp)
+        # gp = self.gap(x)
+        # se = self.se(gp)
         # # output = self.pam0(out+se*out)
-        output = torch.cat([self.pam0(out+se*out), gp.expand(n, c, h, w)], dim=1)
-        output = self.head_conv(output)
+        # output = torch.cat([self.pam0(out+se*out), gp.expand(n, c, h, w)], dim=1)
+        # output = self.head_conv(output)
 
-        # feat4 = F.interpolate(gp, (h, w), mode="bilinear", align_corners=True)
-        # y1 = torch.cat((feat0, feat1, feat2, feat3, feat4), 1)
-        # out = self.project(y1)
-        # output = self.pam0(out)
+        feat4 = F.interpolate(gp, (h, w), mode="bilinear", align_corners=True)
+        y1 = torch.cat((feat0, feat1, feat2, feat3, feat4), 1)
+        out = self.project(y1)
+        output = self.pam0(out)
         return output, gp
 
 
