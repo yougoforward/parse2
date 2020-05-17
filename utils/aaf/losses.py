e8acc9cea511cb56307bc0bfb51ebc8bc7280690
yougoforward <908865817@qq.com>
2020-05-17 16:23:07 +0800

    gnn

--- utils/aaf/losses.py
+++ utils/aaf/losses.py
@@ -176,5 +176,6 @@ def adaptive_affinity_loss(labels,
   not_edge_loss = torch.gather(not_edge_loss, 0, not_edge_indices)
   edge_loss = torch.reshape(edge_loss, (-1,1))
   edge_loss = torch.gather(edge_loss, 0, edge_indices)
-
+  print(edge_loss.mean())
+  print(not_edge_loss.mean())
   return edge_loss, not_edge_loss
