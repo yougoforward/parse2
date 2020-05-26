4ba8992e4b2b770e05ea6f8dbd03f1e5dad698ff
yougoforward <908865817@qq.com>
2020-05-26 11:00:59 +0800

    final_cls

--- utils/gnn_loss.py
+++ utils/gnn_loss.py
@@ -183,29 +183,29 @@ class gnn_loss(nn.Module):
         # # comp_l bce loss 
 
 
-        # # dependency decomposition
-        # # loss_context_att =[]
-        # loss_dp_att = []
-        # for i in range(len(preds[-2])):
-        #     # loss_context = []
-        #     loss_dp = []
-        #     for j in range(self.num_classes-1):
-        #         part_list = self.part_list_list[j]
-        #         parts_onehot = []
-        #         for k in part_list:
-        #             parts_onehot.append(one_hot_pb_list[k+1])
-        #         parts_bg_node = 1-sum(parts_onehot)
-        #         targets_dp_onehot = torch.stack([parts_bg_node] + parts_onehot, dim=1)
-        #         targets_dp = targets_dp_onehot.argmax(dim=1, keepdim=False)
-        #         targets_dp[targets[0] == self.ignore_index] = self.ignore_index
-        #         pred_dp = F.interpolate(input=preds[-2][i][j], size=(h, w), mode='bilinear', align_corners=True)
-        #         loss_dp.append(self.criterion2(pred_dp, targets_dp))
-        #         # pred_dp = F.softmax(input=pred_dp, dim=1)
-        #         # loss_dp.append(lovasz_softmax_flat(*flatten_probas(pred_dp, targets_dp, self.ignore_index),
-        #         #                                    only_present=self.only_present))
-        #     loss_dp = sum(loss_dp)
-        #     loss_dp_att.append(loss_dp)
-        # loss_dp_att = sum(loss_dp_att)
+        # dependency decomposition
+        # loss_context_att =[]
+        loss_dp_att = []
+        for i in range(len(preds[-2])):
+            # loss_context = []
+            loss_dp = []
+            for j in range(self.num_classes-1):
+                part_list = self.part_list_list[j]
+                parts_onehot = []
+                for k in part_list:
+                    parts_onehot.append(one_hot_pb_list[k+1])
+                parts_bg_node = 1-sum(parts_onehot)
+                targets_dp_onehot = torch.stack([parts_bg_node] + parts_onehot, dim=1)
+                targets_dp = targets_dp_onehot.argmax(dim=1, keepdim=False)
+                targets_dp[targets[0] == self.ignore_index] = self.ignore_index
+                pred_dp = F.interpolate(input=preds[-2][i][j], size=(h, w), mode='bilinear', align_corners=True)
+                loss_dp.append(self.criterion2(pred_dp, targets_dp))
+                # pred_dp = F.softmax(input=pred_dp, dim=1)
+                # loss_dp.append(lovasz_softmax_flat(*flatten_probas(pred_dp, targets_dp, self.ignore_index),
+                #                                    only_present=self.only_present))
+            loss_dp = sum(loss_dp)
+            loss_dp_att.append(loss_dp)
+        loss_dp_att = sum(loss_dp_att)
 
         # dsn loss
         pred_dsn = F.interpolate(input=preds[-1], size=(h, w), mode='bilinear', align_corners=True)
