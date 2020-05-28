81aafec0dd149fa1d4cdb5431ffbf7249744945c
yougoforward <908865817@qq.com>
2020-05-28 14:13:07 +0800

    final_cls

--- utils/lovasz_loss.py
+++ utils/lovasz_loss.py
@@ -205,15 +205,15 @@ class AAF_Loss(nn.Module):
                                                          w_not_edge[..., 0],
                                                          self.ignore_index)
         # Apply AAF on 5x5 patch.
-        # eloss_2, neloss_2 = lossx.adaptive_affinity_loss(targets,
-        #                                                  one_hot_lab,
-        #                                                  prob,
-        #                                                  2,
-        #                                                  self.num_classes,
-        #                                                  self.kld_margin,
-        #                                                  w_edge[..., 1],
-        #                                                  w_not_edge[..., 1],
-        #                                                  self.ignore_index)
+        eloss_2, neloss_2 = lossx.adaptive_affinity_loss(targets,
+                                                         one_hot_lab,
+                                                         prob,
+                                                         2,
+                                                         self.num_classes,
+                                                         self.kld_margin,
+                                                         w_edge[..., 1],
+                                                         w_not_edge[..., 1],
+                                                         self.ignore_index)
         # # Apply AAF on 7x7 patch.
         # eloss_3, neloss_3 = lossx.adaptive_affinity_loss(targets,
         #                                                  one_hot_lab,
@@ -226,10 +226,10 @@ class AAF_Loss(nn.Module):
         #                                                  self.ignore_index)
         dec = self.dec
         aaf_loss = torch.mean(eloss_1) * self.kld_lambda_1*dec
-        # aaf_loss += torch.mean(eloss_2) * self.kld_lambda_1*dec
+        aaf_loss += torch.mean(eloss_2) * self.kld_lambda_1*dec
         # aaf_loss += torch.mean(eloss_3) * self.kld_lambda_1*dec
         aaf_loss += torch.mean(neloss_1) * self.kld_lambda_2*dec
-        # aaf_loss += torch.mean(neloss_2) * self.kld_lambda_2*dec
+        aaf_loss += torch.mean(neloss_2) * self.kld_lambda_2*dec
         # aaf_loss += torch.mean(neloss_3) * self.kld_lambda_2*dec
 
         return aaf_loss
