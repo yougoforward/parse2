3bc5e731fa2b28efff2d938b8699b5ff8d55de00
yougoforward <908865817@qq.com>
2020-05-31 20:59:29 +0800

    parse_mod

--- modules/parse_mod.py
+++ modules/parse_mod.py
@@ -8,6 +8,49 @@ from inplace_abn.bn import InPlaceABNSync
 from modules.com_mod import SEModule, ContextContrastedModule
 
 BatchNorm2d = functools.partial(InPlaceABNSync, activation='none')
+class PAM_Module(nn.Module):
+    """ Position attention module"""
+    #Ref from SAGAN
+    def __init__(self, in_dim, key_dim, value_dim, out_dim):
+        super(PAM_Module, self).__init__()
+        self.chanel_in = in_dim
+        # self.pool = nn.MaxPool2d(kernel_size=2)
+
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        # self.gamma = nn.Sequential(nn.Conv2d(in_channels=in_dim, out_channels=1, kernel_size=1, bias=True), nn.Sigmoid())
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X (HxW) X (HxW)
+        """
+        # xp = self.pool(x)
+        xp = x
+        m_batchsize, C, height, width = x.size()
+        m_batchsize, C, hp, wp = xp.size()
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width*height).permute(0, 2, 1)
+        proj_key = self.key_conv(xp).view(m_batchsize, -1, wp*hp)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        # proj_value = self.value_conv(x).view(m_batchsize, -1, width*height)
+        proj_value = xp.view(m_batchsize, -1, wp*hp)
+        
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, height, width)
+        # out = F.interpolate(out, (height, width), mode="bilinear", align_corners=True)
+
+        # gamma = self.gamma(x)
+        # out = (1-gamma)*out + gamma*x
+        out = x+self.gamma*out
+        return out
 
 class ASPPModule(nn.Module):
     """ASPP with OC module: aspp + oc context"""
@@ -40,15 +83,16 @@ class ASPPModule(nn.Module):
                                         InPlaceABNSync(out_dim),
                                         SEModule(out_dim, reduction=16))
 
-        self.psaa_conv = nn.Sequential(nn.Conv2d(in_dim+5 * out_dim, out_dim, 1, padding=0, bias=False),
-                                        InPlaceABNSync(out_dim),
-                                        nn.Conv2d(out_dim, 5, 1, bias=True),
-                                        nn.Sigmoid())
+        # self.psaa_conv = nn.Sequential(nn.Conv2d(in_dim+5 * out_dim, out_dim, 1, padding=0, bias=False),
+        #                                 InPlaceABNSync(out_dim),
+        #                                 nn.Conv2d(out_dim, 5, 1, bias=True),
+        #                                 nn.Sigmoid())
         # self.se = nn.Sequential(nn.Conv2d(out_dim, out_dim, 1, bias=True),
         #                     nn.Sigmoid())
 
         self.project = nn.Sequential(nn.Conv2d(out_dim * 5, out_dim, kernel_size=1, padding=0, bias=False),
                                        InPlaceABNSync(out_dim))
+        self.pam0 = PAM_Module(in_dim=out_dim, key_dim=out_dim//8,value_dim=out_dim,out_dim=out_dim)
     def forward(self, x):
         # parallel branch
         feat0 = self.dilation_0(x)
@@ -63,12 +107,14 @@ class ASPPModule(nn.Module):
         # feat4 = F.interpolate(gp, (h, w), mode="bilinear", align_corners=True)
 
         # psaa
-        y1 = torch.cat((x, feat0, feat1, feat2, feat3, feat4), 1)
-        psaa_att = self.psaa_conv(y1)
-        psaa_att_list = torch.split(psaa_att, 1, dim=1)
+        # y1 = torch.cat((x, feat0, feat1, feat2, feat3, feat4), 1)
+        # psaa_att = self.psaa_conv(y1)
+        # psaa_att_list = torch.split(psaa_att, 1, dim=1)
 
-        y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2, psaa_att_list[3] * feat3, psaa_att_list[4]*feat4), 1)
+        # y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2, psaa_att_list[3] * feat3, psaa_att_list[4]*feat4), 1)
+        y2 = torch.cat((feat0, feat1, feat2, feat3, feat4), 1)
         out = self.project(y2)
+        out = self.pam0(out)
         return out
 
 
