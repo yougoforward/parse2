a21f6037c225a225f4e198b057a3975e17a6e083
yougoforward <908865817@qq.com>
2020-06-05 07:02:04 +0800

    asppmodel

--- modules/parse_mod.py
+++ modules/parse_mod.py
@@ -83,7 +83,7 @@ class ASPPModule(nn.Module):
                                         InPlaceABNSync(out_dim),
                                         SEModule(out_dim, reduction=16))
 
-        self.psaa_conv = nn.Sequential(nn.Conv2d(in_dim+4 * out_dim, out_dim, 1, padding=0, bias=False),
+        self.psaa_conv = nn.Sequential(nn.Conv2d(in_dim+5 * out_dim, out_dim, 1, padding=0, bias=False),
                                         InPlaceABNSync(out_dim),
                                         nn.Conv2d(out_dim, 5, 1, bias=True),
                                         nn.Sigmoid())
@@ -107,17 +107,17 @@ class ASPPModule(nn.Module):
         # feat4 = F.interpolate(gp, (h, w), mode="bilinear", align_corners=True)
 
         # psaa
-        # y1 = torch.cat((x, feat0, feat1, feat2, feat3, feat4), 1)
-        y1 = torch.cat((x, feat0, feat1, feat2, feat3), 1)
+        y1 = torch.cat((x, feat0, feat1, feat2, feat3, feat4), 1)
+        # y1 = torch.cat((x, feat0, feat1, feat2, feat3), 1)
 
         psaa_att = self.psaa_conv(y1)
         psaa_att_list = torch.split(psaa_att, 1, dim=1)
 
-        # y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2, psaa_att_list[3] * feat3, psaa_att_list[4]*feat4), 1)
-        y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2, psaa_att_list[3] * feat3, feat4), 1)
+        y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2, psaa_att_list[3] * feat3, psaa_att_list[4]*feat4), 1)
+        # y2 = torch.cat((psaa_att_list[0] * feat0, psaa_att_list[1] * feat1, psaa_att_list[2] * feat2, psaa_att_list[3] * feat3, feat4), 1)
         # y2 = torch.cat((feat0, feat1, feat2, feat3, feat4), 1)
         out = self.project(y2)
-        out = self.pam0(out)
+        # out = self.pam0(out)
         return out
 
 
