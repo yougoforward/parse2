8edb0719bce5347b15c7afea9d4f0a94b5ada30f
yougoforward <908865817@qq.com>
2020-06-09 19:46:42 +0800

    abrnet

--- train_abrnet.py
+++ train_abrnet.py
@@ -12,12 +12,14 @@ from torch.nn import functional as F
 from torch.nn.parallel.scatter_gather import gather
 from torch.utils import data
 # from dataset.combo_dataloader import DataGenerator
-from dataset.dataloader import DataGenerator
+from dataset.data_pascal import DataGenerator
 # from dataset.datasets import DatasetGenerator
 from network.abrnet import get_model
 # from network.abrnet import get_model
 from progress.bar import Bar
-from utils.lovasz_loss import ABRLovaszLoss
+# from utils.lovasz_loss import ABRLovaszLoss
+from utils.lovasz_loss import lov_AAF_Loss as ABRLovaszLoss
+
 from utils.metric import *
 from utils.parallel import DataParallelModel, DataParallelCriterion
 from utils.visualize import inv_preprocess, decode_predictions
