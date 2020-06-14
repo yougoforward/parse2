473ae17e83b91964e152b172d77449f1d752b5f6
yougoforward <908865817@qq.com>
2020-06-14 09:25:05 +0800

    first commit

--- network/gnn_base_s8.py
+++ network/gnn_base_s8.py
@@ -17,11 +17,16 @@ class DecoderModule(nn.Module):
 
     def __init__(self, num_classes):
         super(DecoderModule, self).__init__()
-        self.conv0 = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, padding=1, dilation=1, bias=False),
-                                   BatchNorm2d(512), nn.ReLU(inplace=False))
-        self.conv1 = nn.Sequential(nn.Conv2d(512, 256, kernel_size=3, padding=1, dilation=1, bias=False),
-                                   BatchNorm2d(256), nn.ReLU(inplace=False))
-
+        # self.conv0 = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, padding=1, dilation=1, bias=False),
+        #                            BatchNorm2d(512), nn.ReLU(inplace=False))
+        # self.conv1 = nn.Sequential(nn.Conv2d(512, 256, kernel_size=3, padding=1, dilation=1, bias=False),
+        #                            BatchNorm2d(256), nn.ReLU(inplace=False))
+        self.conv1 = nn.Sequential(nn.Conv2d(512, 256, kernel_size=3, padding=1, stride=1, bias=False),
+                                   BatchNorm2d(256), nn.ReLU(inplace=False),
+                                   nn.Conv2d(256, 256, kernel_size=1, padding=0, stride=1, bias=False),
+                                   BatchNorm2d(256), nn.ReLU(inplace=False),
+                                   SEModule(256, reduction=16) 
+                                   )
         self.conv2 = nn.Sequential(nn.Conv2d(256, 48, kernel_size=1, stride=1, padding=0, dilation=1, bias=False),
                                    BatchNorm2d(48), nn.ReLU(inplace=False))
 
@@ -35,9 +40,11 @@ class DecoderModule(nn.Module):
 
     def forward(self, xt, xm, xl):
         _, _, h, w = xm.size()
-        xt = self.conv0(F.interpolate(xt, size=(h, w), mode='bilinear', align_corners=True) + self.alpha * xm)
-        _, _, th, tw = xl.size()
-        xt_fea = self.conv1(xt)
+        xt_fea = self.conv1(F.interpolate(xt, size=(h, w), mode='bilinear', align_corners=True) + self.alpha * xm)
+
+        # xt = self.conv0(F.interpolate(xt, size=(h, w), mode='bilinear', align_corners=True) + self.alpha * xm)
+        # _, _, th, tw = xl.size()
+        # xt_fea = self.conv1(xt)
         # xt = F.interpolate(xt_fea, size=(th, tw), mode='bilinear', align_corners=True)
         # xl = self.conv2(xl)
         # x = torch.cat([xt, xl], dim=1)
