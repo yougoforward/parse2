d50dcd662bdcc3624b46532e433b45e51e32cd8f
yougoforward <908865817@qq.com>
2020-06-14 09:55:26 +0800

    first commit

--- network/abrnet3.py
+++ network/abrnet3.py
@@ -6,7 +6,7 @@ from torch.nn import functional as F
 
 from inplace_abn.bn import InPlaceABNSync
 from modules.com_mod import Bottleneck, ResGridNet, SEModule
-from modules.parse_mod import MagicModule, ASPPModule
+from modules.parse_mod import MagicModule, ASPPModule, ASPPModule2
 
 BatchNorm2d = functools.partial(InPlaceABNSync, activation='none')
 
