2c96d41c7e6e64b7996bce3414eff9f7eada8757
yougoforward <908865817@qq.com>
2020-06-17 12:31:31 +0800

    gnnos8

--- modules/parse_mod.py
+++ modules/parse_mod.py
@@ -21,22 +21,22 @@ class ASPPModule(nn.Module):
                                  nn.Conv2d(in_dim, out_dim, 1, bias=False), InPlaceABNSync(out_dim))
 
         self.dilation_0 = nn.Sequential(nn.Conv2d(in_dim, out_dim, kernel_size=1, padding=0, dilation=1, bias=False),
-                                        InPlaceABNSync(out_dim), )
+                                        InPlaceABNSync(out_dim), SEModule(out_dim, reduction=16))
 
         self.dilation_1 = nn.Sequential(nn.Conv2d(in_dim, out_dim, kernel_size=1, padding=0, dilation=1, bias=False),
                                         InPlaceABNSync(out_dim),
                                         nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=6, dilation=6, bias=False),
-                                        InPlaceABNSync(out_dim),)
+                                        InPlaceABNSync(out_dim),SEModule(out_dim, reduction=16))
 
         self.dilation_2 = nn.Sequential(nn.Conv2d(in_dim, out_dim, kernel_size=1, padding=0, dilation=1, bias=False),
                                         InPlaceABNSync(out_dim),
                                         nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=12, dilation=12, bias=False),
-                                        InPlaceABNSync(out_dim), )
+                                        InPlaceABNSync(out_dim), SEModule(out_dim, reduction=16))
 
         self.dilation_3 = nn.Sequential(nn.Conv2d(in_dim, out_dim, kernel_size=1, padding=0, dilation=1, bias=False),
                                         InPlaceABNSync(out_dim),
                                         nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=18, dilation=18, bias=False),
-                                        InPlaceABNSync(out_dim), )
+                                        InPlaceABNSync(out_dim), SEModule(out_dim, reduction=16))
 
         self.psaa_conv = nn.Sequential(nn.Conv2d(in_dim + 5 * out_dim, out_dim, 1, padding=0, bias=False),
                                         InPlaceABNSync(out_dim),
