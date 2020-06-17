c752be5991b1fba32caff4d4e5fd8a3eb6f5d228
yougoforward <908865817@qq.com>
2020-06-17 23:29:22 +0800

    gnn_os8

--- utils/gnn_loss.py
+++ utils/gnn_loss.py
@@ -577,10 +577,8 @@ class gnn_loss_dp(nn.Module):
         # dsn loss
         pred_dsn = F.interpolate(input=preds[-1], size=(h, w), mode='bilinear', align_corners=True)
         loss_dsn = self.criterion(pred_dsn, targets[0])
-        # return 0.33*loss + 0.5*(0.4 * loss_hb + 0.4 * loss_fb) + \
-        #        0.1*(loss_fh_att + loss_up_att + loss_lp_att + loss_com_att + loss_dp_att) + 0.4 * loss_dsn
-        # return (loss + 0.4 * loss_hb + 0.4 * loss_fb)/len(preds[1]) + 0.4 * loss_dsn
-        return (loss + 0.4*loss_hb + 0.4*loss_fb)/len(preds[1]) + 0.4 * loss_dsn + 0.4*(loss_fh_att + loss_up_att + loss_lp_att + loss_com_att + loss_dp_att)/len(preds[1])
+
+        return (loss + 0.4*loss_hb + 0.4*loss_fb)/len(preds[1])+ 0.4 * loss_dsn + 0.1*(loss_fh_att + loss_up_att + loss_lp_att + loss_com_att + loss_dp_att)/len(preds[3])
 
 class bce_gnn_loss(nn.Module):
     """Lovasz loss for Alpha process"""
