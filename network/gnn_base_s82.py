26bf0a98a491a6b3735a97ea5ff2fd974d822e88
yougoforward <908865817@qq.com>
2020-06-18 16:20:28 +0800

    gnn_os8

---
+++ network/gnn_base_s82.py
@@ -0,0 +1,160 @@
+import functools
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from inplace_abn.bn import InPlaceABNSync
+from modules.com_mod import Bottleneck, ResGridNet, SEModule
+from modules.parse_mod import MagicModule, ASPPModule
+from modules.senet import se_resnext50_32x4d, se_resnet101, senet154
+
+BatchNorm2d = functools.partial(InPlaceABNSync, activation='none')
+from modules.convGRU import ConvGRU
+
+class DecoderModule(nn.Module):
+
+    def __init__(self, num_classes):
+        super(DecoderModule, self).__init__()
+        self.conv1 = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, padding=1, stride=1, bias=False),
+                                   BatchNorm2d(512), nn.ReLU(inplace=False),
+                                   nn.Conv2d(512, 256, kernel_size=3, padding=1, stride=1, bias=False),
+                                   BatchNorm2d(256), nn.ReLU(inplace=False),
+                                   SEModule(256, reduction=16) 
+                                   )
+        self.alpha = nn.Parameter(torch.ones(1))
+
+    def forward(self, xt, xm, xl):
+        _, _, h, w = xm.size()
+        xt_fea = self.conv1(F.interpolate(xt, size=(h, w), mode='bilinear', align_corners=True) + self.alpha * xm)
+        return xt_fea
+
+class AlphaDecoder(nn.Module):
+    def __init__(self, hbody_cls):
+        super(AlphaDecoder, self).__init__()
+        self.conv1 = nn.Sequential(nn.Conv2d(512, 256, kernel_size=3, padding=1, stride=1, bias=False),
+                                   BatchNorm2d(256), nn.ReLU(inplace=False),
+                                   nn.Conv2d(256, 256, kernel_size=1, padding=0, stride=1, bias=False),
+                                   BatchNorm2d(256), nn.ReLU(inplace=False),
+                                   SEModule(256, reduction=16) 
+                                   )
+                                   
+        self.alpha_hb = nn.Parameter(torch.ones(1))
+
+    def forward(self, x, skip):
+        _, _, h, w = skip.size()
+
+        xup = F.interpolate(x, size=(h, w), mode='bilinear', align_corners=True)
+        xfuse = xup + self.alpha_hb * skip
+        output = self.conv1(xfuse)
+        return output
+
+
+class GNN_infer(nn.Module):
+    def __init__(self, adj_matrix, upper_half_node=[1, 2, 3, 4], lower_half_node=[5, 6], in_dim=256, hidden_dim=64,
+                 cls_p=7, cls_h=3, cls_f=2):
+        super(GNN_infer, self).__init__()
+        self.cls_p = cls_p
+        self.cls_h = cls_h
+        self.cls_f = cls_f
+        self.in_dim = in_dim
+        self.hidden_dim = hidden_dim
+
+        # node feature transform 
+        self.p_conv = nn.Sequential(
+            nn.Conv2d(in_dim, hidden_dim * cls_p, kernel_size=1, padding=0, stride=1, bias=False),
+            BatchNorm2d(hidden_dim * cls_p), nn.ReLU(inplace=False))
+        self.h_conv = nn.Sequential(
+            nn.Conv2d(in_dim, hidden_dim * cls_h, kernel_size=1, padding=0, stride=1, bias=False),
+            BatchNorm2d(hidden_dim * cls_h), nn.ReLU(inplace=False))
+        self.f_conv = nn.Sequential(
+            nn.Conv2d(in_dim, hidden_dim * cls_f, kernel_size=1, padding=0, stride=1, bias=False),
+            BatchNorm2d(hidden_dim * cls_f), nn.ReLU(inplace=False))
+
+        # node supervision
+        # multi-label classifier
+        # self.f_seg = nn.Sequential(nn.Conv2d(hidden_dim * cls_f, cls_f, 1, groups=cls_f))
+        # self.h_seg = nn.Sequential(nn.Conv2d(hidden_dim * cls_h, cls_h, 1, groups=cls_h))
+        # self.p_seg = nn.Sequential(nn.Conv2d(hidden_dim * cls_p, cls_p, 1, groups=cls_p))
+        self.node_seg = nn.Conv2d(hidden_dim , 1, 1)
+
+    def forward(self, xp, xh, xf):
+        # gnn inference at stride 8
+        # feature transform
+        f_node_list = list(torch.split(self.f_conv(xf), self.hidden_dim, dim=1))
+        h_node_list = list(torch.split(self.h_conv(xh), self.hidden_dim, dim=1))
+        p_node_list = list(torch.split(self.p_conv(xp), self.hidden_dim, dim=1))
+        
+        # node supervision
+        # f_seg = self.f_seg(torch.cat(f_node_list, dim=1))
+        # h_seg = self.h_seg(torch.cat(h_node_list, dim=1))
+        # p_seg = self.p_seg(torch.cat(p_node_list, dim=1))
+
+        f_seg = torch.cat([self.node_seg(node) for node in f_node_list], dim=1)
+        h_seg = torch.cat([self.node_seg(node) for node in h_node_list], dim=1)
+        p_seg = torch.cat([self.node_seg(node) for node in p_node_list], dim=1)
+
+        return [p_seg], [h_seg], [f_seg], [], [], [
+            ], [], [], [], []
+
+
+
+class Decoder(nn.Module):
+    def __init__(self, num_classes=7, hbody_cls=3, fbody_cls=2):
+        super(Decoder, self).__init__()
+        # self.layer5 = MagicModule(2048, 512, 1)
+        self.layer5 = ASPPModule(2048, 512)
+        self.layer6 = DecoderModule(num_classes)
+        self.layerh = AlphaDecoder(hbody_cls)
+        self.layerf = AlphaDecoder(fbody_cls)
+        
+        # adjacent matrix for pascal person 
+        self.adj_matrix = torch.tensor(
+            [[0, 1, 0, 0, 0, 0], [1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 0], [0, 0, 1, 0, 0, 0], [0, 1, 0, 0, 0, 1],
+             [0, 0, 0, 0, 1, 0]], requires_grad=False)
+        
+        # infer with hierarchical person graph
+        self.gnn_infer = GNN_infer(adj_matrix=self.adj_matrix, upper_half_node=[1, 2, 3, 4], lower_half_node=[5, 6],
+                                   in_dim=256, hidden_dim=64, cls_p=7, cls_h=3, cls_f=2)
+        # aux layer
+        self.layer_dsn = nn.Sequential(nn.Conv2d(1024, 256, kernel_size=3, stride=1, padding=1),
+                                       BatchNorm2d(256), nn.ReLU(inplace=False),
+                                       nn.Conv2d(256, num_classes, kernel_size=1, stride=1, padding=0, bias=True))
+
+    def forward(self, x):
+        x_dsn = self.layer_dsn(x[-2])
+        context = self.layer5(x[-1])
+
+        # direct infer
+        p_fea = self.layer6(context, x[1], x[0])
+        h_fea = self.layerh(context, x[1])
+        f_fea = self.layerf(context, x[1])
+
+        # gnn infer
+        p_seg, h_seg, f_seg, decomp_map_f, decomp_map_u, decomp_map_l, comp_map_f, comp_map_u, comp_map_l, \
+        Fdep_att_list= self.gnn_infer(p_fea, h_fea, f_fea)
+
+        return p_seg, h_seg, f_seg, decomp_map_f, decomp_map_u, decomp_map_l, comp_map_f, comp_map_u, comp_map_l, \
+        Fdep_att_list, x_dsn
+
+class OCNet(nn.Module):
+    def __init__(self, block, layers, num_classes):
+        super(OCNet, self).__init__()
+        self.encoder = ResGridNet(block, layers)
+        self.decoder = Decoder(num_classes=num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data)
+            elif isinstance(m, InPlaceABNSync):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+def get_model(num_classes=20):
+    model = OCNet(Bottleneck, [3, 4, 23, 3], num_classes)  # 101
+    return model
