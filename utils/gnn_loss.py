e00b24b6a5c00782f54306df721e701655bb562d
yougoforward <908865817@qq.com>
2020-06-18 09:30:05 +0800

    first commit

--- utils/gnn_loss.py
+++ utils/gnn_loss.py
@@ -380,7 +380,7 @@ class gnn_loss(nn.Module):
         # return 0.33*loss + 0.5*(0.4 * loss_hb + 0.4 * loss_fb) + \
         #        0.1*(loss_fh_att + loss_up_att + loss_lp_att + loss_com_att + loss_dp_att) + 0.4 * loss_dsn
         # return (loss + 0.4 * loss_hb + 0.4 * loss_fb)/len(preds[1]) + 0.4 * loss_dsn
-        return (loss + 0.4*loss_hb + 0.4*loss_fb)/len(preds[1])+ 0.4 * loss_dsn + 0.4*(loss_fh_att + loss_up_att + loss_lp_att + loss_com_att)/len(preds[3])
+        return (loss + 0.4*loss_hb + 0.4*loss_fb)/len(preds[1])+ 0.4 * loss_dsn + 0.1*(loss_fh_att + loss_up_att + loss_lp_att + loss_com_att)/len(preds[3])
 class gnn_loss_dp(nn.Module):
     """Lovasz loss for Alpha process"""
 
