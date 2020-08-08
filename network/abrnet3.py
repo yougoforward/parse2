ae5e3825a741484dc9e04f27a14dc2378e3f1256
yougoforward <908865817@qq.com>
2020-08-08 14:22:13 +0800

    new

--- network/abrnet3.py
+++ network/abrnet3.py
@@ -14,62 +14,15 @@ class DecoderModule(nn.Module):
 
     def __init__(self, num_classes):
         super(DecoderModule, self).__init__()
-        self.conv_m = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, padding=1, bias=False),
-                                   BatchNorm2d(512), nn.ReLU(inplace=False),
-                                   nn.Conv2d(512, 512, kernel_size=1, padding=0, bias=False),
-                                   BatchNorm2d(512), nn.ReLU(inplace=False))
-        self.conv1 = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, padding=1, bias=False),
-                                   BatchNorm2d(512), nn.ReLU(inplace=False),
-                                   nn.Conv2d(512, 256, kernel_size=1, padding=0, bias=False),
-                                   BatchNorm2d(256), nn.ReLU(inplace=False)
-                                   )
-
-        self.conv2 = nn.Sequential(nn.Conv2d(256, 256, kernel_size=1, padding=0, bias=False),
+        self.conv1 = nn.Sequential(nn.Conv2d(512, 256, kernel_size=3, padding=1, bias=False),
                                    BatchNorm2d(256), nn.ReLU(inplace=False))
 
-        self.conv3 = nn.Sequential(nn.Conv2d(256, 256, kernel_size=1, padding=0, bias=False),
-                                   BatchNorm2d(256), nn.ReLU(inplace=False),
-                                   nn.Conv2d(256, 256, kernel_size=1, padding=0, bias=False),
-                                   BatchNorm2d(256), nn.ReLU(inplace=False),
-                                   SEModule(256, reduction=16))
+        self.pred_conv = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(256, num_classes, kernel_size=1, padding=0, dilation=1, bias=True))
 
-        self.conv4 = nn.Conv2d(256, num_classes, kernel_size=1, padding=0, dilation=1, bias=True)
-
-    def forward(self, xt, xm, xl):
-        _, _, h, w = xm.size()
-
-        xt_fea = self.conv1(F.interpolate(xt, size=(h, w), mode='bilinear', align_corners=True) + self.conv_m(xm))
-        _, _, th, tw = xl.size()
-        # xt = F.interpolate(xt_fea, size=(th, tw), mode='bilinear', align_corners=True)
-        # xl = self.conv2(xl)
-        # x_fea = self.conv3(xt+xl)
-        x_seg = self.conv4(xt_fea)
-        return x_seg, xt_fea
-
-
-class AlphaDecoder(nn.Module):
-    def __init__(self, cls):
-        super(AlphaDecoder, self).__init__()
-        self.conv_m = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, padding=1, bias=False),
-                                   BatchNorm2d(512), nn.ReLU(inplace=False),
-                                   nn.Conv2d(512, 512, kernel_size=1, padding=0, bias=False),
-                                   BatchNorm2d(512), nn.ReLU(inplace=False))
-        self.conv1 = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, padding=1, bias=False),
-                                   BatchNorm2d(512), nn.ReLU(inplace=False),
-                                   nn.Conv2d(512, 256, kernel_size=1, padding=0, bias=False),
-                                   BatchNorm2d(256), nn.ReLU(inplace=False),
-                                   SEModule(256, reduction=16) 
-                                   )
-                                   
-        self.cls_hb = nn.Conv2d(256, cls, kernel_size=1, padding=0, stride=1, bias=True)
-        self.alpha_hb = nn.Parameter(torch.ones(1))
-
-    def forward(self, x, skip):
-        _, _, h, w = skip.size()
-        xfuse = self.conv1(F.interpolate(x, size=(h, w), mode='bilinear', align_corners=True) + self.conv_m(skip))
-        output = self.cls_hb(xfuse)
-        return output
-
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.pred_conv(out)
+        return out
 
 
 class Decoder(nn.Module):
@@ -77,22 +30,30 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         # self.layer5 = MagicModule(2048, 512, 1)
         self.layer5 = ASPPModule(2048, 512)
-        self.layer6 = DecoderModule(num_classes)
-        self.layerh = AlphaDecoder(hbody_cls)
-        self.layerf = AlphaDecoder(fbody_cls)
+        self.layer_part = DecoderModule(num_classes)
+        self.layer_half = DecoderModule(hbody_cls)
+        self.layer_full = DecoderModule(fbody_cls)
         
         self.layer_dsn = nn.Sequential(nn.Conv2d(1024, 256, kernel_size=3, stride=1, padding=1),
                                        BatchNorm2d(256), nn.ReLU(inplace=False),
                                        nn.Conv2d(256, num_classes, kernel_size=1, stride=1, padding=0, bias=True))
+
+        self.skip = nn.Sequential(nn.Conv2d(512, 512, kernel_size=1, padding=0, bias=False),
+                                   BatchNorm2d(512), nn.ReLU(inplace=False))
+        self.fuse = nn.Sequential(nn.Conv2d(1024, 512, kernel_size=3, padding=1, bias=False),
+                                   BatchNorm2d(512))
+        self.relu = nn.ReLU()
+
     def forward(self, x):
         x_dsn = self.layer_dsn(x[-2])
+        x[-1] = self.relu(self.fuse(torch.cat([self.skip(x[1]), x[-1]], dim=1))+x[-1])
         seg = self.layer5(x[-1])
 
-        x_seg, xt_fea = self.layer6(seg, x[1], x[0])
-        alpha_hb = self.layerh(seg, x[1])
-        alpha_fb = self.layerf(seg, x[1])
+        seg_part = self.layer_part(seg)
+        seg_half = self.layer_half(seg)
+        seg_full = self.layer_full(seg)
 
-        return [x_seg, alpha_hb, alpha_fb, x_dsn]
+        return [seg_part, seg_half, seg_full, x_dsn]
 
 
 class OCNet(nn.Module):
