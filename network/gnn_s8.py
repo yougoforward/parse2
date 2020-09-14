40ed4cdc7e76af932c9a926548a0119daba5eceb
yougoforward <908865817@qq.com>
2020-09-14 08:39:58 +0800

    gnnloss

--- network/gnn_s8.py
+++ network/gnn_s8.py
@@ -194,6 +194,8 @@ class Dependency(nn.Module):
         self.relation = nn.Sequential(
             nn.Conv2d(2*hidden_dim, hidden_dim, kernel_size=3, padding=1, stride=1, bias=False),
             BatchNorm2d(hidden_dim), nn.ReLU(inplace=False),
+            nn.Conv2d(hidden_dim, hidden_dim, kernel_size=1, padding=0, stride=1, bias=False),
+            BatchNorm2d(hidden_dim), nn.ReLU(inplace=False)
         )
     def forward(self, hv, hu_context, dep_att_huv):
         message= self.project(hu_context*dep_att_huv)
