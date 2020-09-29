65095e570f65c9be45a17e9ea05d0fa1f6dd355f
yougoforward <908865817@qq.com>
2020-09-29 10:36:21 +0800

    parent

--- utils/gnn_loss.py
+++ utils/gnn_loss.py
@@ -729,8 +729,9 @@ class gnn_loss_dp3(nn.Module):
         loss_hb = []
         for i in range(len(preds[1])):
             pred_hb = F.interpolate(input=preds[1][i], size=(h, w), mode='bilinear', align_corners=True)
+            loss_ce = self.criterion(pred_hb, targets[1].long())
             pred_hb = F.softmax(input=pred_hb, dim=1)
-            loss_hb.append(lovasz_softmax_flat(*flatten_probas(pred_hb, targets[1], self.ignore_index),
+            loss_hb.append(loss_ce + lovasz_softmax_flat(*flatten_probas(pred_hb, targets[1], self.ignore_index),
                                       only_present=self.only_present))
         # loss_hb = sum(loss_hb)
 
@@ -739,8 +740,9 @@ class gnn_loss_dp3(nn.Module):
         loss_fb = []
         for i in range(len(preds[2])):
             pred_fb = F.interpolate(input=preds[2][i], size=(h, w), mode='bilinear', align_corners=True)
+            loss_ce = self.criterion(pred_fb, targets[2].long())
             pred_fb = F.softmax(input=pred_fb, dim=1)
-            loss_fb.append(lovasz_softmax_flat(*flatten_probas(pred_fb, targets[2], self.ignore_index),
+            loss_fb.append(loss_ce + lovasz_softmax_flat(*flatten_probas(pred_fb, targets[2], self.ignore_index),
                                       only_present=self.only_present))
         # loss_fb = sum(loss_fb)
 
@@ -860,7 +862,7 @@ class gnn_loss_dp3(nn.Module):
         loss_dsn = self.criterion(pred_dsn, targets[0])
         # return (sum(loss) + 0.4*sum(loss_hb) + 0.4*sum(loss_fb))/(len(preds[0]))+ 0.4 * loss_dsn + 0.1*(loss_fh_att + loss_up_att + loss_lp_att + loss_dp_att + loss_com_att)/(len(preds[3]))
         # return (loss[0] + 0.4*loss_hb[0] + 0.4*loss_fb[0]+loss[-1] + 0.4*loss_hb[-1] + 0.4*loss_fb[-1])+ 0.4 * loss_dsn + 0.2*(loss_fh_att + loss_up_att + loss_lp_att + loss_dp_att + loss_com_att)
-        return 0.5*((loss[0] + 0.4*loss_hb[0] + 0.4*loss_fb[0]+loss[-1] + 0.4*loss_hb[-1] + 0.4*loss_fb[-1])+ 0.4 * loss_dsn)
+        return 0.5*((loss[0] + 0.4*loss_hb[0] + 0.4*loss_fb[0]+loss[-1] + 0.4*loss_hb[-1] + 0.4*loss_fb[-1])) + 0.4 * loss_dsn
 
 class gnn_loss_dp4(nn.Module):
     """Lovasz loss for Alpha process"""
