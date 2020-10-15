517b6161ea8378dd7f8f8dd9c42d853edaff1448
yougoforward <908865817@qq.com>
2020-10-15 11:37:07 +0800

    88

--- network/gnn_s89.py
+++ network/gnn_s89.py
@@ -374,7 +374,7 @@ class Part_Graph(nn.Module):
                 node = self.update[i](decomp, p_node_list[i])
             elif i  in self.lower_part_list:
                 decomp = decomp_l_list[self.lower_part_list.index(i)]
-                height# part_dp = self.part_dp[i-1](p_node_list[i], sum(xpp_list_list[i-1]))
+                # part_dp = self.part_dp[i-1](p_node_list[i], sum(xpp_list_list[i-1]))
                 # node = self.update[i](decomp+part_dp*self.alpha, p_node_list[i])
                 node = self.update[i](decomp, p_node_list[i])
 
